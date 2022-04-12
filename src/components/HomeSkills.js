d53e9e2e4f265b8864eb9877835e6544e4c99f1a
Bruno Minelli <minellibruno@hotmail.com>
2022-04-12 14:31:48 -0300

    Ajustes dos cartões de habilidades do componente HomeSkills

--- src/components/HomeSkills.js
+++ src/components/HomeSkills.js
@@ -9,14 +9,14 @@ class HomeSkills extends Component {
         <h1>Habilidades</h1>
         <section>
           <h2>Hard Skills</h2>
-          <div className={ styles.container_skills}>
-            { skills.hardSkills.map((skill, index) => <p key={`skill-${index}`} className={ styles.container_skills_block }>{skill}</p>) }
+          <div className={ styles.container_skills_block}>
+            { skills.hardSkills.map((skill, index) => <p key={`skill-${index}`} className={ styles.container_skills_card }>{skill}</p>) }
           </div>
         </section>
         <section>
           <h2>Soft Skills</h2>
-          <div className={ styles.container_skills}>
-            { skills.softSkills.map((skill, index) => <p key={`skill-${index}`} className={ styles.container_skills_block }>{skill}</p>) }
+          <div className={ styles.container_skills_block}>
+            { skills.softSkills.map((skill, index) => <p key={`skill-${index}`} className={ styles.container_skills_card }>{skill}</p>) }
           </div>
         </section>
       </article>
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,11 +1,15 @@
 const skills = {
   hardSkills: [
-    'HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js',
-    'RTL', 'TDD', 'Design Responsivo', 'SQL', 'Linux', 'Git'
+    'HTML', 'CSS', 'JavaScript',
+    'React', 'Redux', 'Node.js',
+    'RTL', 'TDD', 'Design Responsivo',
+    'SQL', 'Linux', 'Git'
   ],
   softSkills: [
-    'Gestão de Tempo', 'Senso de Urgência', 'Negociação', 'Pensamento Abstrato',
-    'Empatia', 'Trabalho em Equipe', 'Comunicação Interpessoal', 'Criatividade', 'Aprendizagem Ativa']
+    'Gestão de Tempo', 'Senso de Urgência', 'Negociação',
+    'Pensamento Abstrato', 'Empatia', 'Trabalho em Equipe',
+    'Comunicação Assertiva', 'Criatividade', 'Aprendizagem Ativa',
+    'Liderança', 'Proatividade', 'Colaboração']
 }
 
 export default skills;
