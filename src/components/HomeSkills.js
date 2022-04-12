6eea5aa5941dc289c8c461140cee6f037af35b02
Bruno Minelli <minellibruno@hotmail.com>
2022-04-12 13:36:55 -0300

    Ajuste do css do componente HomeSkills

--- src/components/HomeSkills.js
+++ src/components/HomeSkills.js
@@ -1,14 +1,22 @@
 import{ Component } from 'react';
 import skills from '../data/skills';
+import styles from '../styles/Home.module.css';
 
 class HomeSkills extends Component {
   render() {
     return (
-      <article>
+      <article className={ styles.container_skills }>
+        <h1>Habilidades</h1>
         <section>
-          <h1>Habilidades</h1>
-          <div>
-            { skills.hardSkills.map((skill, index) => <p key={`skill-${index}`}>{skill}</p>) }
+          <h2>Hard Skills</h2>
+          <div className={ styles.container_skills}>
+            { skills.hardSkills.map((skill, index) => <p key={`skill-${index}`} className={ styles.container_skills_block }>{skill}</p>) }
+          </div>
+        </section>
+        <section>
+          <h2>Soft Skills</h2>
+          <div className={ styles.container_skills}>
+            { skills.softSkills.map((skill, index) => <p key={`skill-${index}`} className={ styles.container_skills_block }>{skill}</p>) }
           </div>
         </section>
       </article>
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,11 +1,11 @@
 const skills = {
   hardSkills: [
-    'HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Jest',
+    'HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js',
     'RTL', 'TDD', 'Design Responsivo', 'SQL', 'Linux', 'Git'
   ],
   softSkills: [
     'Gestão de Tempo', 'Senso de Urgência', 'Negociação', 'Pensamento Abstrato',
-    'Empatia', 'Trabalho em Equipe', 'Comunicação Interpessoal', 'Criatividade', 'Autodidata']
+    'Empatia', 'Trabalho em Equipe', 'Comunicação Interpessoal', 'Criatividade', 'Aprendizagem Ativa']
 }
 
 export default skills;
