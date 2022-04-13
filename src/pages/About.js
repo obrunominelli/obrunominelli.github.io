f0fd946f74fe9cec8d9a1ea1184d27f687fd1d2d
Bruno Minelli <minellibruno@hotmail.com>
2022-04-13 12:38:42 -0300

    Criacao do módulo css About

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,21 +3,22 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { about } from '../data/about';
 import { images } from '../data/images';
+import styles from '../styles/About.module.css';
 
 class About extends Component {
   render() {
     return (
       <>
         <Header />
-        <main>
-          <article>
-            <h1>Sobre mim</h1>
+        <main className={ styles.container_about }>
+          <h1>{'</sobre mim>'}</h1>
+          <article className={ styles.container_article }>
             <figure>
-            <img src={images.brunoMinelliPicture.src} alt={images.brunoMinelliPicture.alt}/>
-            <figcaption>
-              <p>Bruno Minelli | Desenvolvedor Web</p>
-            </figcaption>
-          </figure>
+                <img src={images.brunoMinelliPicture.src} alt={images.brunoMinelliPicture.alt} className={ styles.img }/>
+              <figcaption>
+                <p>Bruno Minelli | Desenvolvedor Web</p>
+              </figcaption>
+            </figure>
             { about.map((paragraph, index) => <p key={`paragraph-${index}`}>{paragraph}</p>)}
           </article>
         </main>
