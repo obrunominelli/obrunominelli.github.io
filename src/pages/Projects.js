9995df32237f1f139c67066c24fe75f137ad2c8c
Bruno Minelli <minellibruno@hotmail.com>
2022-04-13 12:17:01 -0300

    Ajustes de CSS e criacao do módulo CSS Projects.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,30 +2,30 @@ import{ Component } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { projects } from '../data/projects';
+import styles from '../styles/Projects.module.css';
 
 class Projects extends Component {
   render() {
     return (
       <>
         <Header />
-          <main>
-            <article>
-              {
-                projects.map((project) => (
-                  <section key={project.title}>
-                    <div>
-                      <h2>{project.title}</h2>
-                      <p>{project.deadline}</p>
-                      <p>{project.description}</p>
-                    </div>
-                    <div>
-                      <h3>Equipe</h3>
-                      {project.team.map((member) => <p key={ member }>{member}</p>)}
-                    </div>
-                  </section>
-                ))
-              }
-            </article>
+          <main className={ styles.container_projects }>
+            <h1>{'</projetos>'}</h1>
+            {
+              projects.map((project) => (
+                <section key={project.title} className={ styles.project_card }>
+                  <div className={ styles.project_info }>
+                    <h2>{project.title}</h2>
+                    <p>{project.deadline}</p>
+                    <p>{project.description}</p>
+                  </div>
+                  <div className={ styles.project_team }>
+                    <h3>Equipe</h3>
+                    {project.team.map((member) => <p key={ member }>{member}</p>)}
+                  </div>
+                </section>
+              ))
+            }
           </main>
         <Footer />
       </>
