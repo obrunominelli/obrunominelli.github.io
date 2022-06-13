e4e833e7d9bfcdb36a3debe9bb09b6904d91963d
Bruno Minelli <obrunominelli@gmail.com>
2022-06-13 12:36:38 -0300

    feat: Criação de cabeçalho e rodapé

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Foot, Paragraph } from '../styles';
+import palette from '../styles/palette';
 
 function Footer() {
   return (
-    <Foot>
+    <Foot
+      background={palette.light.secondary}
+      color={palette.light.primary}
+    >
       <Paragraph>Bruno Minelli&copy; | Desenvolvedor Web</Paragraph>
       <Paragraph>Todos os direitos reservados.</Paragraph>
     </Foot>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Head, NavLink, Title } from '../styles';
+import palette from '../styles/palette';
 
 function Header() {
   return (
-    <Head>
+    <Head
+      background={palette.light.secondary}
+      color={palette.light.primary}
+    >
       <Title>{`</bruno minelli>`}</Title>
-      <NavLink>
+      <NavLink color={palette.light.primary}>
         <Link to='/'>Home</Link>
         <Link to='/projects'>Projetos</Link>
         <Link to='/about'>Sobre</Link>
