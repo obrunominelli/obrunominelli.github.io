9aa7e116bb0bca1b6e7470d82349a7edd2149744
Bruno Minelli <obrunominelli@gmail.com>
2022-06-16 16:38:57 -0300

    feat: Ajustes dos cartões de projetos

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,39 +2,28 @@ import React, { Fragment } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import { projects } from '../data';
-import { Block, Container, Paragraph, Project, Section, Subtitle, Title, Wrapper } from '../styles';
+import { Block, Container, ExternalLink, Paragraph, Project, Subtitle, Wrapper } from '../styles';
 import palette from '../styles/palette';
 
 function Projects() {
   return (
     <Fragment>
       <Header />
-        <Container>
-          <Wrapper>
-            <Section>
-              <Block>
-                { projects.map((project, index) =>
-                  <Project
-                    key={ index }
-                    background={ palette.light.primary }
-                  >
-                    <Title>{ project.title }</Title>
-                    <Paragraph>{ project.description }</Paragraph>
-                    <Paragraph>{ project.stack }</Paragraph>
-                    <Subtitle>Equipe</Subtitle>
-                    <Section>
-                      {project.team
-                        .sort()
-                        .map((member, index) =>
-                        <Paragraph key={ index }>{ member }</Paragraph>
-                      )}
-                    </Section>
-                  </Project>
-                )}
-              </Block>
-            </Section>
-          </Wrapper>
-        </Container>
+      <Container>
+        <Wrapper>
+          <Block>
+            {projects.map((project) =>
+              <Project color={ palette.light.primary }>
+                <Subtitle>{ project.title }</Subtitle>
+                <Paragraph>{ project.description }</Paragraph>
+                <Paragraph>{ project.stack }</Paragraph>
+                <ExternalLink>{ project.url }</ExternalLink>
+                <ExternalLink>{ project.repo }</ExternalLink>
+              </Project>
+            )}
+          </Block>
+        </Wrapper>
+      </Container>
       <Footer />
     </Fragment>
   );
