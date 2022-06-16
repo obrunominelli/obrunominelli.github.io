fb6b1ae80478d3a7de550ca08ea769719a5c52ef
Bruno Minelli <obrunominelli@gmail.com>
2022-06-16 16:43:49 -0300

    style: Ajuste de responsividade de textos

--- src/styles/index.js
+++ src/styles/index.js
@@ -18,7 +18,6 @@ const Head = styled.header `
   }
   
   @media screen and (min-width: 768px) {
-    justify-content: center;
     padding: 2rem 4rem;
 
     h1 {
@@ -29,7 +28,6 @@ const Head = styled.header `
   }
 
   @media screen and (min-width: 1280px) {
-    justify-content: space-between;
     h1 {
       font-size: 3rem;
     }
@@ -45,7 +43,7 @@ const Head = styled.header `
 `;
 
 const NavLink = styled.nav `
-  display: flex;
+  display: none;
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
@@ -55,7 +53,7 @@ const NavLink = styled.nav `
     margin: 0.5rem;
     text-decoration: none;
     color: ${props => props.color};
-    font-size: 1.5rem;
+    font-size: 1.25rem;
     font-weight: 600;
     transition: 0.5s;
 
@@ -79,7 +77,7 @@ const NavSocial = styled.nav `
   display: flex;
   flex-flow: row wrap;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: center;
   width: 100%;
 `;
 
@@ -92,15 +90,15 @@ const ExternalLink = styled.a `
   text-decoration: none;
   color: ${ props => props.color };
   transition: 0.5s;
-
+  
   :hover {
     color: ${ palette.light.accent };
-  }
+  };
 
   img {
-    padding: 0.5rem;
-    width: 5rem;
-  }
+    padding: 0.25rem;
+    width: 4rem;
+  };
 
   @media screen and (min-width: 768px) {
     img {
@@ -141,36 +139,33 @@ const Wrapper = styled.article `
   justify-content: center;
   background-color: ${ props => props.background };
   color: ${ props => props.color };
-  padding: 1rem 2rem;
+  padding: 0.5rem 1rem;
   width: 100%;
 
   @media screen and (min-width: 768px) {
+    padding: 1rem 2rem;
+  };
+
+  @media screen and (min-width: 1280px) {
     padding: 2rem 4rem;
-  }
+  };
 
   @media screen and (min-width: 1920px) {
     padding: 4rem 8rem;
-  }
+  };
 `;
 
 const Section = styled.section `
   display: flex;
   flex-flow: row wrap;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: center;
   width: 100%;
 
   @media screen and (min-width: 768px) {
+    align-items: center;
     flex-flow: row nowrap;
-  }
-
-  @media screen and (min-width: 1280px) {
-    width: 90%;
-  }
-
-  @media screen and (min-width: 1920px) {
-    width: 85%;
-  }
+  };
 `;
 
 const Block = styled.div `
@@ -178,13 +173,12 @@ const Block = styled.div `
   flex-flow: row wrap;
   align-items: flex-start;
   justify-content: center;
-  min-width: 50%;
-  max-width: 100%;
+  width: 100%;
 `;
 
 const Title = styled.h1 `
   color: ${ props => props.color };
-  font-size: 1.75rem;
+  font-size: 1.5rem;
   text-align: center;
   margin-bottom: 0.5rem;
   width: 100%;
@@ -195,7 +189,7 @@ const Title = styled.h1 `
   }
 
   @media screen and (min-width: 1280px) {
-    font-size: 2.75rem;
+    font-size: 3rem;
   }
 
   @media screen and (min-width: 1920px) {
@@ -212,22 +206,23 @@ const Subtitle = styled.h2 `
 
   @media screen and (min-width: 768px) {
     text-align: left;
-    font-size: 1.25rem;
+    font-size: 1.5rem;
   }
 
   @media screen and (min-width: 1280px) {
-    font-size: 2.5rem;
+    font-size: 1.75rem;
   }
 
   @media screen and (min-width: 1920px) {
-    font-size: 3rem;
+    font-size: 2.75rem;
   }
 `;
 
 const Paragraph = styled.p `
-  font-size: 0.875rem;
+  font-size: 0.75rem;
   margin-bottom: 0.5rem;
   color: ${props => props.color};
+  width: fit-content;
 
   a {
     color: ${ props => props.color };
@@ -245,7 +240,7 @@ const Paragraph = styled.p `
   }
 
   @media screen and (min-width: 768px) {
-    font-size: 0.875rem;
+    font-size: 1rem;
     text-align: left;
   }
 
@@ -272,119 +267,45 @@ const Card = styled.div `
   }
 
   @media screen and (min-width: 768px) {
-    max-width: 25%;
-
     ${Title}, ${Paragraph} {
       text-align: center;
-    }
-  }
-`;
-
-const Dropdown = styled(Card) `
-  border: 0.0625rem solid ${palette.light.primary};
-  margin: 0.5rem;
-  padding: 0.5rem;
-  width: 45%;
-  transition: 1s;
-  
-  ${Block} {
-    display: none;
-    padding: 1rem;
-    top: 3.75rem;
-    transform: 1s;
-  }
-
-  ${Paragraph} {
-    text-align: left;
-  }
-
-  :hover {
-    color: ${palette.light.secondary};
-    background-color: ${palette.light.primary};
-    cursor: pointer;
-
-    ${Paragraph} {
-      color: ${palette.light.secondary};
-    }
-
-    ${Block} {
-      display: block;
-    }
-  }
+    };
+  };
 `;
 
 const Project = styled.div `
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-start;
-  color: ${ palette.light.primary };
-  border: 0.25rem solid ${ palette.light.primary };
-  border-radius: 0.4rem;
-  padding: 1rem;
+  justify-content: center;
+  border: 0.0625rem solid ${ props => props.color };
+  border-radius: 0.5rem;
+  color: ${ props => props.color };
   margin: 0.5rem;
-  width: 100%;
-  transition: 0.5s;
+  padding: 1rem;
+  min-width: 45%;
+  max-width: 100%;
+  transition: 1s;
 
-  ${ Section } {
-    justify-content: center;
-    width: 100%;
-    
-    ${ Paragraph } {
-      text-align: center;
-      margin: 0.5rem;
-      width: 45%;
-    };
-  };
-  
-  ${Title}, ${Subtitle} {
-    text-align: center;
-    border-bottom: 0.25rem solid ${palette.light.accent};
+  ${ Subtitle } {
+    border-bottom: 0.25rem solid ${ palette.light.accent };
     width: fit-content;
-  }
+  };
+
+  :hover {
+    background-color: ${ palette.light.primary };
+    color: ${ palette.light.secondary };
+  };
 
   @media screen and (min-width: 768px) {
-    justify-content: center;
-    width: 45%;
-    height: 25rem;
-
-    ${ Section } {
-      flex-flow: row wrap;
-
-      ${ Paragraph } {
-        width: 40%;
-      };
-    }
-
-    ${Title}, ${Subtitle} {
-      margin-bottom: 0.5rem;
-    }
-  }
-
-  @media screen and (min-width: 1280px) {
-    margin: 1.5rem;
-    padding: 2rem;
-    width: 45%;
-    height: 40rem;
-
-    :hover {
-      background-color: ${ palette.light.primary };
-      color: ${ palette.light.secondary };
-    }
-
-    ${Title}, ${Subtitle} {
-      margin-bottom: 1rem;
-    }
-  }
+    width: 30%;
+    height: 15rem;
+  };
 
   @media screen and (min-width: 1920px) {
-    width: 45%;
-    min-height: 60rem;
-
-    ${Title}, ${Subtitle} {
-      margin-bottom: 1.5rem;
-    }
-  }
+    width: 30%;
+    height: 40rem;
+  };
 `;
 
 const Thumbnail = styled.figure `
@@ -396,8 +317,8 @@ const Thumbnail = styled.figure `
 
 const Image = styled.img `
   padding: 1.5rem;
-  min-width: 50%;
-  max-width: 100%;
+  min-width: 40%;
+  max-width: 80%;
 `;
 
 const Foot = styled.footer `
@@ -439,7 +360,6 @@ export {
   Section,
   Block,
   Card,
-  Dropdown,
   Project,
   Title,
   Subtitle,
