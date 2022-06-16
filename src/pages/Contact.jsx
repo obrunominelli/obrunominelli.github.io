55251c2a8aeac0b232491b80c09f8f8a7363689c
Bruno Minelli <obrunominelli@gmail.com>
2022-06-16 16:37:21 -0300

    fix: Componente retorna à fase de planejamento.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,7 @@
 import React, { Fragment } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import { images, services, social } from '../data';
-import { Block, Container, Dropdown, ExternalLink, Image, NavSocial, Paragraph, Section, Title, Wrapper } from '../styles';
-import palette from '../styles/palette';
+import { Container, Wrapper } from '../styles';
 
 function Contact() {
   return (
@@ -11,44 +9,6 @@ function Contact() {
       <Header />
       <Container>
         <Wrapper>
-          <Title color={ palette.light.primary }>
-            No que eu posso te ajudar?
-          </Title>
-          <Section>
-            <Block>
-              {services.map(({title, description}, index) =>
-                <Dropdown key={ index }>
-                  <Paragraph>{ title }</Paragraph>
-                  <Block>
-                    <Paragraph>{ description }</Paragraph>
-                  </Block>
-                </Dropdown>
-              )}
-            </Block>
-          </Section>
-        </Wrapper>
-        <Wrapper
-          background={ palette.light.primary }
-          color={ palette.light.secondary }
-        >
-          <Section>
-            <Block>
-              <NavSocial>
-                <ExternalLink href={ social.linkedin } target="_blank">
-                  <Image src={ images.linkedin.light.src } alt={ images.linkedin.light.alt } />
-                </ExternalLink>
-                <ExternalLink href={social.github} target="_blank">
-                  <Image src={ images.github.light.src } alt={ images.github.light.alt } />
-                </ExternalLink>
-                <ExternalLink href={social.email} target="_blank">
-                  <Image src={ images.email.light.src } alt={ images.email.light.alt } />
-                </ExternalLink>
-                <ExternalLink href={ social.telegram } target="_blank">
-                  <Image src={ images.telegram.light.src } alt={ images.telegram.light.alt } />
-                </ExternalLink>
-              </NavSocial>
-            </Block>
-          </Section>
         </Wrapper>
       </Container>
       <Footer />
