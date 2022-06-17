eb2e2d88d178cf33da6eda571b5c3255fd4a139c
Bruno Minelli <obrunominelli@gmail.com>
2022-06-17 18:32:47 -0300

    feat: Remoção da barra de navegação interna

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Head, NavLink, Title } from '../styles';
+import { Head, Title } from '../styles';
 import palette from '../styles/palette';
 
 function Header() {
@@ -10,14 +9,8 @@ function Header() {
       color={ palette.light.primary }
     >
       <Title>{ `</bruno minelli>` }</Title>
-      <NavLink color={ palette.light.primary }>
-        <Link to='/'>Home</Link>
-        <Link to='/projects'>Projetos</Link>
-        <Link to='/about'>Sobre</Link>
-        <Link to='/contact'>Contato</Link>
-      </NavLink>
     </Head>
-  )
-}
+  );
+};
 
 export default Header;
