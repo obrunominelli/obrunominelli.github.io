9fc6a2429eb846e5e8f3d0413e0b4bd6276bf94e
Bruno Minelli <obrunominelli@gmail.com>
2022-06-27 14:47:19 -0300

    feat: Criação do diretório styles

---
+++ src/styles/themes/index.js
@@ -0,0 +1,13 @@
+const dark = {
+  primary: '#0B0133',
+  secondary: '#F6F5FA',
+  accent: '#D2C8FA',
+};
+
+const light = {
+  primary: '#F6F5FA',
+  secondary: '#0B0133',
+  accent: '#643EFA',
+};
+
+export { dark, light };
